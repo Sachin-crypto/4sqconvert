b3c374f124b156d40565d20305bde042923628b5
Severin Schols <severin@schols.de>
2013-11-28 14:49:18 +0100

    initial commit

---
+++ convert.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+from icalendar import Calendar
+import os
+import codecs
+
+directory = os.path.dirname(__file__)
+cal = Calendar.from_ical(open(os.path.join(directory, 'import.ics'),'r').read())
+
+for comp in cal.walk():
+	if comp.name == "VEVENT":
+		name = comp["location"]
+		time = comp["dtstart"].dt.strftime("%x %X")
+		latitude = comp["geo"].latitude
+		longitude = comp["geo"].longitude
+		url = comp["url"]
+		print ",".join([name,  time,  unicode(latitude), unicode(longitude) , url])
